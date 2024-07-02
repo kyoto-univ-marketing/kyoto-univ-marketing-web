1808c82c36731578c3c20893f4a40073b30f2de7
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-02 15:26:18 +0900

    フォーム送信前の確認ダイアログの表示を実装

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,10 +1,20 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { FormInput } from '../FormInput/FormInput'
 import { FormTextarea } from '../FormTextarea/FormTextarea'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from '../ui/alert-dialog'
 import { Button } from '../ui/button'
 import { Form } from '../ui/form'
 
@@ -37,20 +47,52 @@ export const ContactForm: FC<ContactFormProps> = ({ ...props }) => {
     const form = useForm<ContactFormSchema>({
         resolver: zodResolver(schema),
     })
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+    const sendForm = () => {
+        console.log(form.getValues())
+    }
+
     return (
-        <Form {...form}>
-            <form
-                className='space-y-8'
-                onSubmit={form.handleSubmit(
-                    () => false, // TODO
-                )}
-            >
-                <FormInput control={form.control} name='name' label='お名前' />
-                <FormInput control={form.control} name='affiliation' label='ご所属' />
-                <FormInput control={form.control} name='email' label='メールアドレス' />
-                <FormTextarea control={form.control} name='message' label='お問い合わせ内容' />
-                <Button type='submit'>送信</Button>
-            </form>
-        </Form>
+        <>
+            <Form {...form}>
+                <form className='space-y-8' onSubmit={form.handleSubmit(() => setDialogOpen(true))}>
+                    <FormInput control={form.control} name='name' label='お名前' />
+                    <FormInput control={form.control} name='affiliation' label='ご所属' />
+                    <FormInput control={form.control} name='email' label='メールアドレス' />
+                    <FormTextarea control={form.control} name='message' label='お問い合わせ内容' />
+                    <Button type='submit'>送信</Button>
+                </form>
+            </Form>
+            <ConfirmDialog open={dialogOpen} onOpenChange={setDialogOpen} onConfirm={sendForm} />
+        </>
+    )
+}
+
+const ConfirmDialog = ({
+    open,
+    onOpenChange,
+    onConfirm,
+}: {
+    open: boolean
+    onOpenChange: (open: boolean) => void
+    onConfirm: () => void
+}) => {
+    return (
+        <AlertDialog open={open} onOpenChange={onOpenChange}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your account and remove your data
+                        from our servers.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     )
 }
