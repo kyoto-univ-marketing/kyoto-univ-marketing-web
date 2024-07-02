cdf8a1fa2c44a252b0f90d00c52025a8d92ec147
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-02 14:36:17 +0900

    FromInputコンポーネントを実装した

---
+++ src/components/FormInput/FormInput.stories.tsx
@@ -0,0 +1,74 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { FormInput } from './FormInput'
+import { Button } from '../ui/button'
+import { Form } from '../ui/form'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+    title: 'FormInput',
+    component: FormInput,
+    tags: ['autodocs'],
+    args: {},
+} satisfies Meta<typeof FormInput>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    render: ({ label, description, placeholder }) => {
+        const form = useForm<{ name: string }>()
+        return (
+            <Form {...form}>
+                <FormInput
+                    control={form.control}
+                    name='name'
+                    label={label}
+                    placeholder={placeholder}
+                    description={description}
+                />
+            </Form>
+        )
+    },
+    args: { label: 'お名前', placeholder: '山田 太郎', description: 'お名前を入力してください', name: 'name' },
+}
+
+export const Email: Story = {
+    render: ({ label, description, placeholder }) => {
+        const schema = z.object({
+            email: z
+                .string({
+                    required_error: 'メールアドレスは必須です',
+                })
+                .email({
+                    message: 'メールアドレスの形式が正しくありません',
+                }),
+        })
+        const form = useForm<{ email: string }>({
+            resolver: zodResolver(schema),
+        })
+        return (
+            <Form {...form}>
+                <form className='space-y-4' onSubmit={form.handleSubmit((data) => console.log(data))}>
+                    <FormInput
+                        control={form.control}
+                        name='email'
+                        label={label}
+                        placeholder={placeholder}
+                        description={description}
+                    />
+                    <Button type='submit'>送信</Button>
+                </form>
+            </Form>
+        )
+    },
+    args: {
+        label: 'メールアドレス',
+        placeholder: '****@example.com',
+        description: 'メールアドレスを入力してください',
+        name: 'email',
+    },
+}
---
+++ src/components/FormInput/FormInput.tsx
@@ -0,0 +1,39 @@
+import { FieldPath, FieldValues, UseControllerProps } from 'react-hook-form'
+
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+
+export interface FormInputProps<
+    TFieldValues extends FieldValues = FieldValues,
+    TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> extends UseControllerProps<TFieldValues, TName> {
+    label?: string
+    placeholder?: string
+    description?: string
+}
+
+export const FormInput = <
+    TFieldValues extends FieldValues = FieldValues,
+    TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+    label,
+    placeholder,
+    description,
+    ...props
+}: FormInputProps<TFieldValues, TName>) => {
+    return (
+        <FormField
+            {...props}
+            render={({ field }) => (
+                <FormItem>
+                    {label && <FormLabel>{label}</FormLabel>}
+                    <FormControl>
+                        <Input {...field} placeholder={placeholder} />
+                    </FormControl>
+                    {description && <FormDescription>{description}</FormDescription>}
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    )
+}
