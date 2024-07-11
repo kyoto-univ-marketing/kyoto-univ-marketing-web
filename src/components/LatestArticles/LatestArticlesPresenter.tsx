b92d32cbdc70152f1205801ea858a2876c782d34
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-11 18:06:16 +0900

    feat: 新着記事コンポーネントを実装した

---
+++ src/components/LatestArticles/LatestArticlesPresenter.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react'
+
+import { NextLink } from '../NextLink/NextLink'
+import { Button } from '../ui/button'
+
+export interface LatestArticle {
+    title: string
+    date: string
+    slug: string
+}
+
+export interface LatestArticlesPresenterProps {
+    articleList: LatestArticle[]
+}
+
+/** 新着記事の表示を行うコンポーネント。取得は`LatestArticles`を用いる */
+export const LatestArticlesPresenter: FC<LatestArticlesPresenterProps> = ({ articleList, ...props }) => {
+    return (
+        <div className='space-y-6 p-4'>
+            {/* TODO: コンポーネントを分離する */}
+            <h2 className='text-2xl'>新着記事</h2>
+            <table className='table-auto border-separate border-spacing-x-2 border-spacing-y-3'>
+                <tbody>
+                    {articleList.map((article) => (
+                        <tr key={article.slug}>
+                            <td className='align-top'>{article.date}</td>
+                            <td className='whitespace-pre-wrap'>
+                                <NextLink href={`/articles/${article.slug}`}>{article.title}</NextLink>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className='mx-auto w-fit'>
+                <Button asChild>
+                    <NextLink href='/articles'>活動記録を見る</NextLink>
+                </Button>
+            </div>
+        </div>
+    )
+}
