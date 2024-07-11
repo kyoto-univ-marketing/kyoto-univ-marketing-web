67e0c028ffa91dcb2e590ea4b5f5e74e15ced564
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-11 11:18:27 +0900

    ロゴを実装

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import { FC } from 'react'
+
+import logo from '@/../public/logo.png'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+import { AvatarProps } from '../ui/avatar'
+
+export interface LogoProps {
+    size?: AvatarProps['size']
+}
+
+export const Logo: FC<LogoProps> = ({ ...props }) => {
+    return (
+        <Avatar {...props}>
+            <AvatarImage asChild src={logo.src}>
+                <Image src={logo} alt='logo' fill />
+            </AvatarImage>
+            <AvatarFallback>logo</AvatarFallback>
+        </Avatar>
+    )
+}
