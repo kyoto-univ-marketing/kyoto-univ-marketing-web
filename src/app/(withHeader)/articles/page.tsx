179f6ae6370b716bc28d946fcf985551f1a00025
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-13 15:12:25 +0900

    記事一覧の取得と表示

--- src/app/(withHeader)/articles/page.tsx
+++ src/app/(withHeader)/articles/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return <></>
+import { ArticleCardList } from '@/components/ArticleCardList/ArticleCardList'
+import { getActivityList } from '@/lib/microcms'
+
+export default async function Page() {
+    const activities = await getActivityList()
+    return (
+        <>
+            <ArticleCardList
+                cardList={activities.contents.map((activity) => {
+                    return {
+                        title: activity.title,
+                        slug: activity.id,
+                        date: new Date(activity.publishedAt),
+                        image: activity.thumbnail.url,
+                    }
+                })}
+            />
+        </>
+    )
 }
