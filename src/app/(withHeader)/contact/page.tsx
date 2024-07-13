e2f6bd2f6f3691ca5b314cf5780c37af599687b7
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-13 18:06:10 +0900

    メタデータの追加

--- src/app/(withHeader)/contact/page.tsx
+++ src/app/(withHeader)/contact/page.tsx
@@ -4,6 +4,7 @@ import { ContactPage } from '@/components/ContactPage/ContactPage'
 
 export const metadata: Metadata = {
     title: 'お問い合わせ',
+    description: 'お問い合わせページです。ご自由にお問い合わせください。',
 }
 
 export default function Page() {
--- src/app/(withHeader)/contact/success/page.tsx
+++ src/app/(withHeader)/contact/success/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next'
+
 import { ContactSuccessPage } from '@/components/ContactSuccessPage/ContactSuccessPage'
+
+export const metadata: Metadata = {
+    title: 'お問い合わせ完了',
+    description: 'お問い合わせ完了ページです。',
+}
 
 export default function Page() {
     return (
