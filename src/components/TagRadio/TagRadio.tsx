2b45a75314764d0bf6f56a777ee59a1dc2caa15c
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-14 10:23:10 +0900

    shadcnのRadioGroupを利用することにした

--- src/components/TagRadio/TagRadio.tsx
+++ src/components/TagRadio/TagRadio.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react'
 import { cn } from '@/lib/utils'
 
 import { badgeVariants } from '../ui/badge'
+import { Label } from '../ui/label'
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
 
 export interface TagRadioOption {
     readonly label?: string
@@ -11,21 +13,23 @@ export interface TagRadioOption {
 
 export interface TagRadioItemProps extends TagRadioOption {
     selected: boolean
-    onClick?: () => void
 }
 
-export const TagRadioItem: FC<TagRadioItemProps> = ({ label, value, selected, onClick, ...props }) => {
+export const TagRadioItem: FC<TagRadioItemProps> = ({ label, value, selected, ...props }) => {
     return (
-        <button
-            onClick={onClick}
-            className={cn(
-                !selected && 'hover:bg-slate-100',
-                selected && '',
-                badgeVariants({ variant: selected ? 'default' : 'outline' }),
-            )}
-        >
-            {label || value}
-        </button>
+        <div>
+            <RadioGroupItem hidden id={value} value={value}></RadioGroupItem>
+            <Label
+                htmlFor={value}
+                className={cn(
+                    !selected && 'hover:bg-slate-100',
+                    selected && '',
+                    badgeVariants({ variant: selected ? 'default' : 'outline' }),
+                )}
+            >
+                {label || value}
+            </Label>
+        </div>
     )
 }
 
@@ -38,16 +42,15 @@ export interface TagRadioProps {
 /** タグの単一選択 */
 export const TagRadio: FC<TagRadioProps> = ({ value, onChange, options, ...props }) => {
     return (
-        <div className='flex flex-wrap gap-2'>
+        <RadioGroup className='flex flex-wrap' onValueChange={onChange}>
             {options.map((option) => (
                 <TagRadioItem
                     key={option.value}
                     label={option.label}
                     value={option.value}
                     selected={option.value === value}
-                    onClick={() => onChange(option.value)}
                 />
             ))}
-        </div>
+        </RadioGroup>
     )
 }
