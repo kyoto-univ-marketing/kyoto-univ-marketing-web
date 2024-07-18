3212d2ec45d1cc3248d3e259e3f98ea028933849
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-18 10:47:38 +0900

    サークルについてページを組み立てた

--- src/app/(withHeader)/about/page.tsx
+++ src/app/(withHeader)/about/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from 'next'
+
+import { AboutPage } from '@/components/about/AboutPage/AboutPage'
+import { pageLinkObject } from '@/constants/pageLinks'
+
+export const metadata: Metadata = {
+    title: pageLinkObject.ABOUT.text,
+}
+
 export default function Page() {
-    return <></>
+    return (
+        <main>
+            <AboutPage />
+        </main>
+    )
 }
