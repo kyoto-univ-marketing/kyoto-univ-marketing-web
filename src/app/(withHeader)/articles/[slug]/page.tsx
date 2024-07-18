45f331f09ddac7c76760afb229dd0e5722fd5e5b
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-18 21:14:37 +0900

    メタデータ作成処理を追加した

--- src/app/(withHeader)/articles/[slug]/page.tsx
+++ src/app/(withHeader)/articles/[slug]/page.tsx
@@ -1,12 +1,26 @@
+import { Metadata } from 'next'
+
 import { ArticleDetailsPage } from '@/components/article-details/ArticleDetailsPage/ArticleDetailsPage'
 import { getActivityById, getActivityIds } from '@/lib/microcms'
+
+interface Params {
+    slug: string
+}
 
 export const generateStaticParams = async () => {
     const allContentIds = await getActivityIds()
     return allContentIds.map((slug) => ({ slug }))
 }
+export const generateMetadata = async ({ params }: { params: Params }): Promise<Metadata> => {
+    const { slug } = params
+    const { title, description } = await getActivityById(slug) // TODO: キャッシュが効かないので対策する
+    return {
+        title,
+        description,
+    }
+}
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Params }) {
     const { slug } = params
     const content = await getActivityById(slug)
     return (
