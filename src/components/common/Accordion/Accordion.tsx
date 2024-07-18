5b5ff4699e13e0c77a43523b81889d85591531d4
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-18 10:53:32 +0900

    アコーディオンのデザイン修正

--- src/components/common/Accordion/Accordion.tsx
+++ src/components/common/Accordion/Accordion.tsx
@@ -1,5 +1,7 @@
 import { FC, ReactNode } from 'react'
 
+import { cn } from '@/lib/utils'
+
 import { AccordionContent, AccordionItem, AccordionTrigger, Accordion as ShadcnAccordion } from '../../ui/accordion'
 
 export interface AccordionItem {
@@ -10,15 +12,16 @@ export interface AccordionItem {
 
 export interface AccordionProps {
     items: AccordionItem[]
+    contentClassName?: string
 }
 
-export const Accordion: FC<AccordionProps> = ({ items, ...props }) => {
+export const Accordion: FC<AccordionProps> = ({ items, contentClassName, ...props }) => {
     return (
         <ShadcnAccordion type='multiple' className='w-full'>
             {items.map(({ value, title, content }) => (
-                <AccordionItem key={value} value={value}>
-                    <AccordionTrigger>{title}</AccordionTrigger>
-                    <AccordionContent>{content}</AccordionContent>
+                <AccordionItem className='border-b-0' key={value} value={value}>
+                    <AccordionTrigger className='border-b'>{title}</AccordionTrigger>
+                    <AccordionContent className={cn('py-8', contentClassName)}>{content}</AccordionContent>
                 </AccordionItem>
             ))}
         </ShadcnAccordion>
