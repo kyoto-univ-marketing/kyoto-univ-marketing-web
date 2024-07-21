a8dcecaad12c047f3ca5a05096f796883ddbc9f0
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-21 22:31:01 +0900

    delete todo comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,7 +73,7 @@ const config = {
                 'accordion-up': 'accordion-up 0.2s ease-out',
             },
             fontFamily: {
-                title: ['var(--font-hina-mincho)', 'BIZ UDPMincho', 'serif'], // TODO: 正式なフォントを設定する
+                title: ['var(--font-hina-mincho)', 'BIZ UDPMincho', 'serif'],
             },
             fontSize: {
                 title: '1.7857142857rem',
