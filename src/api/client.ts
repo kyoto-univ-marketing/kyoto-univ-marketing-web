e62b5223f7d3993ba298214312c668db5ef62e18
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-28 12:04:25 +0900

    URLは公開してもいいのでベタ書きに変更

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,10 +2,8 @@ import createClient from 'openapi-fetch'
 
 import { paths } from './schema'
 
-if (!process.env.BACKEND_BASE_URL) {
-    throw new Error('BACKEND_BASE_URL is not set')
-}
+const baseUrl = 'https://web-backend-nnxvtvdboa-an.a.run.app/'
 
 export const client = createClient<paths>({
-    baseUrl: process.env.BACKEND_BASE_URL,
+    baseUrl,
 })
