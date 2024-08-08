6bc81c05e80610065e85a90f97b28e767639570d
ayu0616 <ayumuogawa0616@gmail.com>
2024-08-08 17:58:31 +0900

    記事のog画像を設定

--- src/app/(withHeader)/articles/[slug]/page.tsx
+++ src/app/(withHeader)/articles/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+import { cache } from 'react'
 
 import { ArticleDetailsPage } from '@/components/article-details/ArticleDetailsPage/ArticleDetailsPage'
 import { getActivityById, getActivityIds } from '@/lib/microcms'
@@ -8,22 +9,30 @@ interface Params {
     slug: string
 }
 
+const getActivity = cache(async (slug: string) => getActivityById(slug))
+
 export const generateStaticParams = async () => {
     const allContentIds = await getActivityIds()
     return allContentIds.map((slug) => ({ slug }))
 }
 export const generateMetadata = async ({ params }: { params: Params }): Promise<Metadata> => {
     const { slug } = params
-    const { title, description } = await getActivityById(slug).catch(notFound) // TODO: キャッシュが効かないので対策する
+    const { title, description, thumbnail } = await getActivity(slug).catch(notFound)
     return {
         title,
         description,
+        openGraph: {
+            images: [{ ...thumbnail, alt: title }],
+        },
+        twitter: {
+            images: [{ ...thumbnail, alt: title }],
+        },
     }
 }
 
 export default async function Page({ params }: { params: Params }) {
     const { slug } = params
-    const content = await getActivityById(slug).catch(notFound)
+    const content = await getActivity(slug).catch(notFound)
     return (
         <>
             <ArticleDetailsPage {...content} />
