156441a3ea5b5e3057ebf2e29c35d21aa33f86e5
ayu0616 <ayumuogawa0616@gmail.com>
2024-09-01 10:26:35 +0900

    プロジェクト一覧にPPR導入

--- src/components/projects/ProjectPage/ProjectPage.tsx
+++ src/components/projects/ProjectPage/ProjectPage.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { FC, Suspense } from 'react'
+import { FaSpinner } from 'react-icons/fa6'
 
 import { client } from '@/api/client'
 import { NextLink } from '@/components/common/NextLink/NextLink'
@@ -12,39 +13,52 @@ export interface ProjectPageProps {
 }
 
 export const ProjectPage: FC<ProjectPageProps> = async ({ projectDescription, ...props }) => {
-    const projects = (await client.GET('/api/project/')).data ?? []
     return (
         <>
             <PageTitle>プロジェクト</PageTitle>
             <p className='mb-16 px-12'>{projectDescription}</p>
             <div className='mb-16 space-y-16'>
-                <div className='bg-backgroundSecondary pb-12'>
-                    <ProjectList
-                        projects={projects.filter((pr) => pr.tag === 'マーケティング支援')}
-                        tag='マーケティング支援'
-                    />
-                </div>
-                <div className='pb-12'>
-                    <ProjectList
-                        projects={projects.filter((pr) => pr.tag === '共同プロジェクト')}
-                        tag='共同プロジェクト'
-                    />
-                    <Button
-                        asChild
-                        className='mx-auto mt-12 flex h-fit w-fit max-w-[75%] flex-wrap items-center justify-center text-lg'
-                    >
-                        <NextLink href='/contact'>
-                            <span className='inline-block'>共同プロジェクトに関する</span>
-                            <span className='inline-block'>お問い合わせはこちら</span>
-                        </NextLink>
-                    </Button>
-                </div>
-                <div className='bg-backgroundSecondary pb-12'>
-                    <ProjectList
-                        projects={projects.filter((pr) => pr.tag === 'オリジナルプロジェクト')}
-                        tag='オリジナルプロジェクト'
-                    />
-                </div>
+                <Suspense
+                    fallback={
+                        <div className='flex h-24 w-full items-center justify-center p-8'>
+                            <FaSpinner className='h-8 w-8 animate-spin' />
+                        </div>
+                    }
+                >
+                    <Projects />
+                </Suspense>
+            </div>
+        </>
+    )
+}
+
+const Projects = async () => {
+    const projects = (await client.GET('/api/project/')).data ?? []
+    return (
+        <>
+            <div className='bg-backgroundSecondary pb-12'>
+                <ProjectList
+                    projects={projects.filter((pr) => pr.tag === 'マーケティング支援')}
+                    tag='マーケティング支援'
+                />
+            </div>
+            <div className='pb-12'>
+                <ProjectList projects={projects.filter((pr) => pr.tag === '共同プロジェクト')} tag='共同プロジェクト' />
+                <Button
+                    asChild
+                    className='mx-auto mt-12 flex h-fit w-fit max-w-[75%] flex-wrap items-center justify-center text-lg'
+                >
+                    <NextLink href='/contact'>
+                        <span className='inline-block'>共同プロジェクトに関する</span>
+                        <span className='inline-block'>お問い合わせはこちら</span>
+                    </NextLink>
+                </Button>
+            </div>
+            <div className='bg-backgroundSecondary pb-12'>
+                <ProjectList
+                    projects={projects.filter((pr) => pr.tag === 'オリジナルプロジェクト')}
+                    tag='オリジナルプロジェクト'
+                />
             </div>
         </>
     )
