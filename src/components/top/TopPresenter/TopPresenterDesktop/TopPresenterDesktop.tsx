b425ba70c76e5aa282813bc6e37b8b96b6547df2
ayu0616 <ayumuogawa0616@gmail.com>
2025-02-05 11:25:48 +0900

    feat: デスクトップ用のトップページのデザインを反映

--- src/components/top/TopPresenter/TopPresenterDesktop/TopPresenterDesktop.tsx
+++ src/components/top/TopPresenter/TopPresenterDesktop/TopPresenterDesktop.tsx
@@ -1,17 +1,11 @@
-import { PageImage } from '@/components/common/PageImage/PageImage'
-import Link from 'next/link'
 import { FC } from 'react'
 import { Suspense } from 'react'
-
-import img3884 from '../../../../../public/page-images/top/IMG_3884.webp'
-import img3893 from '../../../../../public/page-images/top/IMG_3893.webp'
 import img3767 from '../../../../../public/top-header/IMG_3767.webp'
 import img3816 from '../../../../../public/top-header/IMG_3816.webp'
 import img3878 from '../../../../../public/top-header/IMG_3878.webp'
 import img6855 from '../../../../../public/top-header/IMG_6855.webp'
 import img9802 from '../../../../../public/top-header/IMG_9802.webp'
 import { Logo } from '../../../common/Logo/Logo'
-import { Button } from '../../../ui/button'
 import { ImageSwitch } from '../../ImageSwitch/ImageSwitch'
 import { LatestArticles } from '../../LatestArticles/LatestArticles'
 import { LatestArticlesSkeleton } from '../../LatestArticlesSkeleton/LatestArticlesSkeleton'
@@ -27,51 +21,46 @@ const imageList = [img3878, img3767, img3816, img6855, img9802]
 export const TopPresenterDesktop: FC<TopPresenterDesktopProps> = ({ message, subMessage, ...props }) => {
     return (
         <div className='relative w-full'>
-            <ImageSwitch
-                className='absolute -z-50 brightness-50 h-[calc(100svh-var(--spacing)*18)]'
-                imageList={imageList}
-                interval={7000}
-                transitionDuration={2000}
-            />
-            <div className='flex h-[calc(100svh-var(--spacing)*18)] flex-col'>
-                <div className='flex w-full flex-1 select-none flex-col items-center justify-between text-white'>
-                    <div className='flex flex-col items-center space-y-6 p-6 pt-16'>
-                        <div>
-                            <Logo reverse size='3xl' />
-                        </div>
-                        <h1 className='w-fit px-6 font-title text-3xl font-bold'>
-                            <span className='inline-block'>京大</span>
-                            <span className='inline-block'>マーケティング研究所</span>
-                        </h1>
-                    </div>
-                    <div className='flex w-full flex-col space-y-8'>
-                        <Button
-                            asChild
-                            className='w-fit min-w-[66.7%] self-center rounded-none p-6 text-primary'
-                            variant='outline'
-                        >
-                            <Link href='/about'>サークルについて</Link>
-                        </Button>
-                        <div className='mb-8! w-fit max-w-[75%] bg-primary px-12 py-8 text-primary-foreground'>
-                            <h2 className='ml-4 text-xl'>{message}</h2>
-                        </div>
-                    </div>
+            <div className='fixed w-full top-18 left-0 -z-50 h-[calc(100svh-var(--spacing)*18)]'>
+                <div className='relative size-full'>
+                    <ImageSwitch
+                        className='size-full'
+                        imageList={imageList}
+                        interval={7000}
+                        transitionDuration={2000}
+                    />
+                    <div className='bg-white/75 size-full absolute top-0 left-0'></div>
                 </div>
             </div>
-            <div className='p-12'>
+            <div className='flex h-[calc(100svh-var(--spacing)*18)] justify-center items-center lg:p-16 lg:gap-16 p-8 gap-8'>
+                <div className='flex justify-center items-center w-full max-w-1/3'>
+                    <h1 className='font-title text-3xl font-bold w-fit'>
+                        <span className='inline-block'>京大</span>
+                        <span className='inline-block'>マーケティング</span>
+                        <span className='inline-block'>研究所</span>
+                    </h1>
+                </div>
+                <div className='flex-1 h-full relative'>
+                    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 aspect-square w-full max-w-[33vh]'>
+                        <Logo className='size-full' />
+                    </div>
+                </div>
+                <div className='flex justify-center items-center w-full max-w-1/3'>
+                    <div className='font-title text-3xl font-bold w-fit'>Kyodai Marketing Institute</div>
+                </div>
+            </div>
+            {/* <div className='p-12'>
                 <p className='whitespace-pre-wrap text-sm'>{subMessage}</p>
-            </div>
-            <div className='mb-16'>
-                <Suspense fallback={<LatestArticlesSkeleton />}>
-                    <LatestArticles />
-                </Suspense>
-            </div>
-            <div className='mb-16 grid gap-4'>
-                <PageImage alt='勉強会の様子' containerClassName='w-3/4 aspect-64/27' src={img3884} />
-                <PageImage alt='勉強会の教室' containerClassName='w-1/2 justify-self-end' src={img3893} />
-            </div>
-            <div className='mb-16'>
-                <TopPageProject />
+            </div> */}
+            <div className='grid grid-cols-2 mb-24 px-8 gap-8'>
+                <div className='bg-white/75 rounded-md pb-8'>
+                    <Suspense fallback={<LatestArticlesSkeleton />}>
+                        <LatestArticles />
+                    </Suspense>
+                </div>
+                <div className='bg-white/75 rounded-md pb-8'>
+                    <TopPageProject />
+                </div>
             </div>
         </div>
     )
