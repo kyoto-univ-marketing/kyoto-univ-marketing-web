cdf55f5ed2bf586d3dfbedea3eb287a48b2902f3
ayu0616 <ayumuogawa0616@gmail.com>
2025-02-05 10:23:35 +0900

    feat: トップページにローディングスピナーを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Metadata } from 'next'
 import { OrganizationJsonLD } from '@/components/common/JsonLD/OrganizationJsonLD'
 import { TopPage } from '@/components/top/TopPage/TopPage'
 import { TOP_PAGE_DESCRIPTION } from '@/constants/description'
+import { Suspense } from 'react'
+import { FaSpinner } from 'react-icons/fa6'
 
 export const metadata: Metadata = {
     description: TOP_PAGE_DESCRIPTION,
@@ -12,7 +14,17 @@ export default function Home() {
     return (
         <>
             <main>
-                <TopPage />
+                <Suspense
+                    fallback={
+                        <div className='grid h-screen w-full justify-center items-center'>
+                            <div className='flex h-24 w-full items-center justify-center p-8'>
+                                <FaSpinner className='h-8 w-8 animate-spin' />
+                            </div>
+                        </div>
+                    }
+                >
+                    <TopPage />
+                </Suspense>
             </main>
             <OrganizationJsonLD />
         </>
